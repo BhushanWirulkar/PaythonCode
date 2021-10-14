dbd0667cf9c040ff53beba5e9e6144e097dd4d61
BhushanWirulkar <90099030+BhushanWirulkar@users.noreply.github.com>
2021-10-14 23:58:33 +0530

    Add files via upload

---
+++ stringmethods.py
@@ -0,0 +1,8 @@
+strName = "MY Name is bhushan"
+print(strName.upper())
+print(strName.lower())
+print(strName.title())
+print(strName.find('is'))
+print(strName.replace('a','e'))
+print("bhushan" in strName)
+print("Bhushan" in strName)
