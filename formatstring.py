d41b903f97411969a8cde929a04d37961c30ab8a
BhushanWirulkar <90099030+BhushanWirulkar@users.noreply.github.com>
2021-10-14 23:43:22 +0530

    Add files via upload

---
+++ formatstring.py
@@ -0,0 +1,8 @@
+fname = "Bhushan"
+lname="Wirulkar"
+print1 = f'{fname} {lname} is developer'
+print2 = f'{fname} :)  is cartoonist'
+print3 = f'{fname[0].lower()}{lname[0:]} '
+print(print1)
+print(print2)
+print(print3)
