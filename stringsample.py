ca846f03b86ca620d7f997ad41d43dd0a66b124b
BhushanWirulkar <90099030+BhushanWirulkar@users.noreply.github.com>
2021-10-14 23:15:03 +0530

    Sample string program

---
+++ stringsample.py
@@ -0,0 +1,14 @@
+strcomment = '''
+My name is bhushan.
+My age is 36 year.
+I live at Pune.
+'''
+strArr = "This is sample string to test"
+
+print(strcomment)
+print(strArr[:])
+print(strArr[1:])
+print(strArr[:-1])
+print(strArr[3:-1])
+print(strArr[3:-3])
+print(strArr[-15:-1])
