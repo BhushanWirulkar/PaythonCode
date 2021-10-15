406d3e27dcca21993113cdd422061fe7ff222e39
bwirulkar-processia <bwirulkar@processia.com>
2021-10-16 00:27:05 +0530

    Removed unwanted code

--- Input1.py
+++ Input1.py
@@ -4,11 +4,5 @@ by=input("What is your birth year? ")
 age = 2021 - int(by)
 #print(name+" likes "+colour)
 print(name+" age is "+str(age))
-print("HAPPY BIRTHDAY   ^")
-print("HAPPY BIRTHDAY  ()")
-print("                ||")
-print("            -----------")
-print("             |       |")
-print("             |       |")
-print("            ----------")
+
 
